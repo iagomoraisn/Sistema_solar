43981bbe3cc8ba969db674c8af2eed7bb7d101fc
iagomoraisn <iagojkd96@hotmail.com>
2023-05-31 18:08:12 -0300

    req5

---
+++ src/components/PlanetCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type PlanetCardProps = {
+  planetName: string;
+  planetImage: string;
+};
+
+function PlanetCard({ planetName, planetImage }: PlanetCardProps) {
+  return (
+    <div data-testid="planet-card">
+    <p data-testid="planet-name">{planetName}</p>
+    <img src={planetImage} alt={`Planeta ${planetName}`} />
+    </div>
+  );
+};
+
+export default PlanetCard;
