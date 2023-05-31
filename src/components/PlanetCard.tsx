54b8ebb635e3fc8e67d457f11db5c179e94361e1
iagomoraisn <iagojkd96@hotmail.com>
2023-05-31 18:15:23 -0300

    req5.1

--- src/components/PlanetCard.tsx
+++ src/components/PlanetCard.tsx
@@ -8,10 +8,10 @@ type PlanetCardProps = {
 function PlanetCard({ planetName, planetImage }: PlanetCardProps) {
   return (
     <div data-testid="planet-card">
-    <p data-testid="planet-name">{planetName}</p>
-    <img src={planetImage} alt={`Planeta ${planetName}`} />
+      <p data-testid="planet-name">{planetName}</p>
+      <img src={ planetImage } alt={ `Planeta ${planetName} ` } />
     </div>
   );
-};
+}
 
 export default PlanetCard;
